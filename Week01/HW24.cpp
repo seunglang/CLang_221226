ee897202800527d6f049c9b86cca9e455d6fa647
test <test@test.com>
2023-01-06 00:37:21 +0900

    달팽이 문제 풀이

--- Week01/HW24.cpp
+++ Week01/HW24.cpp
@@ -5,39 +5,34 @@ int input();
 int calc(int);
 void output(int, int);
 int main() {
-	int length = input();
-	calc(length);
-	int i = calc(length);
-	output(length, i);
+	int depth = input();
+	calc(depth);
+	int i = calc(depth);
+	output(depth, i);
 }
 
 int input() {
-	int length;
+	int depth;
 	printf("우물의 깊이를 입력하시오(cm단위) : ");
-	scanf("%d", &length);
-	return length;
+	scanf("%d", &depth);
+	return depth;
 }
-int calc(int length) {
-	int afternoon = 0;
-	int day = 24;
-	int time;
-	int i = 0;
+int calc(int depth) {
+	int height = 0;
+	int i = 1;
 	// i가 (1)일 역할 수행
 	while (true) {
-		length = length - 50;
-		if (afternoon % day == 0 || length <= 0) {
-			i++;
-			day += 24;
-		}
-		afternoon = afternoon + 12;
-		if (length <= 0) {
+		height += 50;
+		if (height >= depth) {
 			break;
 		}
-		length = length + 30;
-		afternoon = afternoon + 12;
+		else {
+			height -= 20;
+			i++;
+		}
 	}
 	return i;
 }
-void output(int length, int i) {
-	printf("우물높이 %dcm : %d", length, i);
+void output(int depth, int i) {
+	printf("우물높이 %dcm : %d", depth, i);
 }
